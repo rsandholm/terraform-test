403b64e4f119d50682690cb1769b7a6cec708651
rsandholm <96058014+rsandholm@users.noreply.github.com>
2023-03-08 22:16:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "rg_name" {
+  description = "Name of the resource group to be created"
+  type        = string
+}
 
+variable "location" {
+  description = "Region in which to create the resources. Defaults to `West Europe`"
+  type        = string
+  default     = "westeurope"
+}
