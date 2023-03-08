18cfe14d9c1375083bab6714e85d4ce0d7603d2d
rsandholm <96058014+rsandholm@users.noreply.github.com>
2023-03-08 22:19:45 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,10 @@
-
+terraform {
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/44158903/terraform/state/blog-production"
+    lock_address   = "https://gitlab.com/api/v4/projects/44158903/terraform/state/blog-production/lock"
+    lock_method    = "POST"
+    unlock_address = "https://gitlab.com/api/v4/projects/44158903/terraform/state/blog-production/lock"
+    unlock_method  = "DELETE"
+    retry_wait_min = 5
+  }
+}
